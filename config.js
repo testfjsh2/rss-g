e4602a515be859a8981b6cb029f19a7c2943c3c6
fjshadows <nikita_z@progforce.com>
2016-01-23 01:13:22 +0300

    fix db url

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 var config = {
-  "connection": "mongodb://localhost/rss",
+  "connection": "mongodb://ds045694.mongolab.com:45694/rss",
   "defaultUrl": "http://www.mk.ru/rss/index.xml"
 };
 
+// "connection": "mongodb://localhost/rss",
 module.exports = config;
